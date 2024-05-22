d8a77f72966fa5bf3c01c73f2bf5da9357c1bc00
singhAmandeep007 <amandeeps0803@gmail.com>
2024-05-22 23:58:49 +0530

    Updates

---
+++ precache-manifest.720cb68f60961e68dceeb43e643279e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a2b82f3280bdd5170ab90544b17566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0436bdfa30aa82495d1",
+    "url": "./static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d98395e3322cd2689c06",
+    "url": "./static/css/main.9448b3c2.chunk.css"
+  },
+  {
+    "revision": "c0436bdfa30aa82495d1",
+    "url": "./static/js/2.a7995a4d.chunk.js"
+  },
+  {
+    "revision": "540a081da9e1f61ae2ea5926ad550139",
+    "url": "./static/js/2.a7995a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98395e3322cd2689c06",
+    "url": "./static/js/main.6b8c2b1d.chunk.js"
+  },
+  {
+    "revision": "cfe50918ac3b1a1095cd",
+    "url": "./static/js/runtime-main.b7dd659a.js"
+  }
+]);
